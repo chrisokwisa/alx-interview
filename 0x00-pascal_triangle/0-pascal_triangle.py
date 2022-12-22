87c2299126c292ec954ea2b2e1286d19c257f0d6
Christabel Okwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-12-22 12:22:51 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" Gets the function of the Pascal's triangle"""
+
+
 def pascal_triangle(n):
+    """That returns a list of lists of integers
+representing the Pascal's triangle """
     if n <= 0:
         return []
-    res = []
-    row = []
-    for i in range(n):
-        # temp = res[1] + [0]
-        row = [1]
-        if i > 0:
-            for j in range(i):
-                row.append(sum(res[-1][j:j+2]))
+    
+    res = [[1]]
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
         res.append(row)
     return res
