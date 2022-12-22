2a33632c39cb2d84720d18ae9bf210ad9e17e7fa
chrisokwisa <okwisachristabel@gmail.com>
2022-12-22 00:43:59 -0800

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,6 +9,6 @@ def pascal_triangle(n):
         row = [1]
         if i > 0:
             for j in range(len(res[-1] + 1)):
-                row.append(temp[j + 1])
+                row.append(temp[j] + temp[j + 1])
         res.append(row)
     return res
