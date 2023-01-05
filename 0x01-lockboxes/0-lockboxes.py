9ff08b1d6c551753539061f97c31786ee50d7964
chrisokwisa <okwisachristabel@gmail.com>
2023-01-05 15:10:47 +0000

    Determines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Contains the code that determines if each box contains keys to the other boxes
+""" determines if each box contains keys to the other boxes
 """
 
 
@@ -18,21 +18,7 @@ def canUnlockAll(boxes):
             if j not in visited:
                 dfs(j)
 
-    # start the depth-first search to explore all the boxes that can be reached from the first box, False otherwise
+    # start the depth-first
     dfs(0)
-    
+
     return len(visited) == len(boxes)
-
-
-# This function works by using a depth-first search to explore all the boxes that can be reached from the first box (which is unlocked).
-# If the depth-first search visits all the boxes, it means that all the boxes can be opened and the function returns True.
-# Otherwise, the function returns False.
-
-
-# The time complexity of the canUnlockAll function is O(n), where n is the number of boxes.
-# This is because the function performs a depth-first search, which involves visiting each box at most once.
-
-# The space complexity of the function is also O(n), as the function stores the visited boxes in a set, which takes O(n) space.
-
-# Note that this is assuming that each box contains at most a constant number of keys. If the number of keys in each box is not constant,
-# the time and space complexity of the function would be dependent on the number of keys as well.
