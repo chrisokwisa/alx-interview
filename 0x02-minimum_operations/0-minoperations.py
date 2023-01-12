74e94397eb4c5a3d6b2752afa1b7c511e0fb7d45
chrisokwisa <okwisachristabel@gmail.com>
2023-01-12 10:24:04 +0300

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Gets the minimum operations """
+
+
+def minoperations(n):
+    """ calculates the fewest number of operations
+    needed to result in n H charaters"""
+    if n <= 0:
+        return 0
+    operations = 0
+    i = 1
+    while i < n:
+        i *= 2
+        operations += 1
+    return operations
