f8fcf1d3372cf20382f91608075b51a7098603ca
chrisokwisa <okwisachristabel@gmail.com>
2023-01-12 07:44:25 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,7 @@
 def minOperations(n):
     """ calculates the fewest number of operations
     needed to result in n H charaters """
-    if n  0:
+    if n <= 0:
         return 0
     operations = 0
     i = 1
