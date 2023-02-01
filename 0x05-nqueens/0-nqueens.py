cdff5f8dc0d227175f128ac0e94005e231881e45
chrisokwisa <okwisachristabel@gmail.com>
2023-02-01 02:03:49 -0800

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,71 +1,47 @@
 #!/usr/bin/python3
-""" Contain a script that gets the nQueens """
-
-
 import sys
 
+def nqueens(n):
+    # function to solve N queens problem
+    # implementation of backtracking algorithm
+    def is_not_under_attack(row, col):
+        # check if a queen can be placed in cell (row, col)
+        for i in range(row):
+            if board[i] == col or \
+               abs(board[i] - col) == abs(i - row):
+                return False
+        return True
 
-def is_not_attacked(board, row, col, N):
-    # check if there is a queen in the same row or col
+    def solve(row):
+        if row == n:
+            result.append(board[:])
+            return
+        for col in range(n):
+            if is_not_under_attack(row, col):
+                board[row] = col
+                solve(row + 1)
 
-    for i in range(N):
-        if board[row][i] or board[i][col]:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, N, 1)):
-        if board[i][j]:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, N, 1)):
-        if board[i][j]:
-            return False
-
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    return True
-
-
-def solve(board, col, N, solutions):
-    if col >= N:
-        solutions.append([(i, j) for i,
-                          row in enumerate(board) for j, val
-                          in enumerate(row) if val])
-        return
-
-    for i in range(N):
-        if is_not_attacked(board, i, col, N):
-            board[i][col] = 1
-            solve(board, col + 1, N, solutions)
-            board[i][col] = 0
-
-
-def nqueens(N):
-    if type(N) != int:
+    if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
-
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
-    solve(board, 0, N, solutions)
-
-    for solution in solutions:
-        print(solution)
-
+    board = [-1] * n
+    result = []
+    solve(0)
+    return result
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
+    try:
         n = int(sys.argv[1])
-        nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    result = nqueens(n)
+    for sol in result:
+        print(sol)
