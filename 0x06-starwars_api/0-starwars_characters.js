b935b8815283a35150656fcc3571c7c236421bca
chrisokwisa <okwisachristabel@gmail.com>
2023-02-06 13:17:10 +0000

    Star wars charaters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const https = require('https');
+
+// The first positional argument passed is the Movie ID
+const movieId = process.argv[2];
+
+// API URL to retrieve details of the specified movie
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+https.get(apiUrl, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    const movie = JSON.parse(data);
+    const characters = movie.characters;
+    characters.forEach((character) => {
+      https.get(character, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          const characterDetails = JSON.parse(data);
+          console.log(characterDetails.name);
+        });
+      });
+    });
+  });
+});
