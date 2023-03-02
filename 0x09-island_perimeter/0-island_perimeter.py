82862e47a2328cf1b7e421cc1ab8e68ab1280e93
chrisokwisa <okwisachristabel@gmail.com>
2023-03-02 09:40:12 +0000

    Island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,15 +12,15 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
 
-                if row > 0 and grid[row - 1][col] == 1:
+                if row != 0 and grid[row - 1][col] == 1:
                     perimeter -= 1
 
-                if col > 0 and grid[col - 1][row] == 1:
+                if col != 0 and grid[row][col - 1] == 1:
                     perimeter -= 1
 
-                if row != 0 and grid[row + 1][col] == 1:
+                if row + 1 != rows and grid[row + 1][col] == 1:
                     perimeter -= 1
-                if col != 0 and grid[col + 1][row] == 1:
+                if col + 1 != cols and grid[row][col + 1] == 1:
                     perimeter -= 1
 
     return perimeter
