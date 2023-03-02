6ef49dc7fd9267e1236c5c478c23b5549af54792
chrisokwisa <okwisachristabel@gmail.com>
2023-03-02 10:52:23 +0300

    Island island_perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Should be an executable file """
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for col in range(cols):
+        for row in range(row):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+
+                if col > 0 and grid[col - 1][row] == 1:
+                    perimeter -= 2
+
+    return perimeter
